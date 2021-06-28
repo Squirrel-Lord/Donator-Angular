09a760eb271da37b29c8fc0d58d4683bef646dff
Ross Baldwin <baldwinr17@gmail.com>
2021-06-28 17:48:54 -0500

    Started listing out basic fields from smart contract.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DonationDetailComponent } from './donation-detail/donation-detail.component';
-import { HomeComponent } from './home/home.component';
+import { HelpComponent } from './help/help.component';
 import { MakeDonationComponent } from './make-donation/make-donation.component';
+import { ReceiverComponent } from './receiver/receiver.component';
 
 const routes: Routes = [
   { path: 'donation-details', component: DonationDetailComponent },
   { path: 'make-donation', component: MakeDonationComponent },
-  {path: '', redirectTo: '/donation-details', pathMatch: 'full'},
+  { path: 'receiver', component: ReceiverComponent },
+  { path: 'help', component: HelpComponent },
+  { path: '', redirectTo: '/donation-details', pathMatch: 'full' },
 ];
 
 @NgModule({
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,29 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DonationDetailComponent } from './donation-detail/donation-detail.component';
 import { HomeComponent } from './home/home.component';
-import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
-import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
 import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
-import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
-import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
-import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
-import { MdbPopoverModule } from 'mdb-angular-ui-kit/popover';
-import { MdbRadioModule } from 'mdb-angular-ui-kit/radio';
-import { MdbRangeModule } from 'mdb-angular-ui-kit/range';
-import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
-import { MdbScrollspyModule } from 'mdb-angular-ui-kit/scrollspy';
 import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
-import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
-import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MakeDonationComponent } from './make-donation/make-donation.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ReceiverComponent } from './receiver/receiver.component';
 import { HelpComponent } from './help/help.component';
+import { MatCardModule } from '@angular/material/card'
 
 @NgModule({
   declarations: [
@@ -33,26 +21,15 @@ import { HelpComponent } from './help/help.component';
     MakeDonationComponent,
     NavbarComponent,
     ReceiverComponent,
-    HelpComponent
+    HelpComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MdbCarouselModule,
-    MdbCheckboxModule,
     MdbCollapseModule,
-    MdbDropdownModule,
-    MdbFormsModule,
-    MdbModalModule,
-    MdbPopoverModule,
-    MdbRadioModule,
-    MdbRangeModule,
-    MdbRippleModule,
-    MdbScrollspyModule,
     MdbTabsModule,
-    MdbTooltipModule,
-    MdbValidationModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
